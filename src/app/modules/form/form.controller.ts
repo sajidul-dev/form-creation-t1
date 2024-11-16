5617aed6e5f8b4dda5fefb36519f408d6400a3ed
sajidul-dev <mdsajidulalam1245@gmail.com>
2024-11-17 02:53:59 +0600

    tesing

--- src/app/modules/form/form.controller.ts
+++ src/app/modules/form/form.controller.ts
@@ -3,6 +3,8 @@ import catchAsync from "../../../shared/catchAsyncs";
 const getFormInfo = catchAsync(async (req, res, next) => {
   // const { formName, formActive } = req.body;
   // console.log(formName, formActive);
+  const { ...fromdata } = req.body;
+  console.log(fromdata, "Request from api");
   res.status(200).json({
     data: req.body,
     success: true,
