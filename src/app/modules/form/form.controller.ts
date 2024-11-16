b02426b778b12d792e4285ce471a29bc67fc58ce
sajidul-dev <mdsajidulalam1245@gmail.com>
2024-11-17 02:04:03 +0600

    Deploy to vercel confid added

--- src/app/modules/form/form.controller.ts
+++ src/app/modules/form/form.controller.ts
@@ -4,6 +4,7 @@ const getFormInfo = catchAsync(async (req, res, next) => {
   const { formName, formActive } = req.body;
   console.log(formName, formActive);
   res.status(200).json({
+    data: { formName, formActive },
     success: true,
     message: "Form created successfully",
   });
