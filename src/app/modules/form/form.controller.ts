b791b3f2a710f738e6e865b3d5697c7856fbe412
sajidul-dev <mdsajidulalam1245@gmail.com>
2024-11-17 02:09:07 +0600

    Deploy to vercel config

--- src/app/modules/form/form.controller.ts
+++ src/app/modules/form/form.controller.ts
@@ -4,7 +4,7 @@ const getFormInfo = catchAsync(async (req, res, next) => {
   const { formName, formActive } = req.body;
   console.log(formName, formActive);
   res.status(200).json({
-    data: { formName, formActive },
+    data: { formName: formName, formActive: formActive },
     success: true,
     message: "Form created successfully",
   });
