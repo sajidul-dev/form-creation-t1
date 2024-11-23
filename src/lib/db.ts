8700544209599d25ff3ee2cd1a9aedc91c608062
sajidul-dev <mdsajidulalam1245@gmail.com>
2024-11-24 00:37:38 +0600

    dialact module added

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,6 +3,7 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize("E-commerce", "root", "", {
   host: "localhost",
   dialect: "mysql",
+  dialectModule: require("mysql2"),
 });
 
 export default sequelize;
